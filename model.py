c253e0913e4d4f7f3b22489ca6736dd70977901e
Wouter van de Vijver <woutervandevijver@hotmail.com>
2022-06-27 18:27:33 +0200

    big model

--- model.py
+++ model.py
@@ -1,20 +1,24 @@
 import numpy as np
-
+from keras.layers import TextVectorization
 from keras.utils import pad_sequences
 
 
 class RnnModel:
 
-    def __init__(self, model, vc, max_len):
+    def __init__(self, model, vocab, max_len):
         self.model = model
-        self.vc = vc
+        self.vc = TextVectorization(
+            vocabulary = ['startseq'] + vocab + ['endseq']
+        )
         self.max_len = max_len
 
     def predict(self, image):
         desc = ['startseq']
-        for i in range(36):   
-            desc_tok = pad_sequences([self.vc(desc)], maxlen=self.max_len).reshape((1,36))
+        for i in range(self.max_len+2):   
+            desc_tok = pad_sequences([self.vc(desc)], maxlen=self.max_len+2).reshape((1,self.max_len+2))
             pred = self.model.predict([image, desc_tok], verbose=0)
             pred_text = self.vc.get_vocabulary()[np.argmax(pred)]
+            if pred_text == 'endseq':
+                break
             desc.append(pred_text)
         return desc
--- text_prep.py
+++ text_prep.py
@@ -1,10 +1,16 @@
 import joblib 
+from typing import List
 import os
 import spacy
 
 import regex as re
 
-def load_descriptions(path):
+def load_descriptions(path:str) -> dict:
+    '''
+    Function that loads all the descriptions of all images from a txt file and return a dictionary where the keys are the image IDs and the items the descriptions
+
+    :param path: str that represents the path to the text file of descriptions
+    '''
     with open(path) as file:
         text = file.read()
 
@@ -19,7 +25,12 @@ def load_descriptions(path):
 
     return caption_dict
 
-def clean_descriptions(caption_dict):
+def clean_descriptions(caption_dict: dict) -> dict:
+    '''
+    Function that cleans all captions in a dictionary
+    
+    :param caption_dict: dictionary that needs to be cleaned
+    '''
     nlp = spacy.load('en_core_web_sm')
     clean_captions = {}
     for key in caption_dict.keys():
@@ -30,13 +41,23 @@ def clean_descriptions(caption_dict):
             clean_captions[key].append(lemmas)
     return clean_captions
 
-def to_vocabulary(captions):
+def to_vocabulary(captions: dict) -> List[str]:
+    '''
+    Function that a list of all words appearing in the given dictionary of descriptions
+    
+    :param captions: dictionary of descriptions
+    '''
     vocabulary = set()
     for key in captions.keys():
         [vocabulary.update(word) for word in captions[key]]
-    return vocabulary
+    return list(vocabulary)
 
-def load_dataset(path):
+def load_dataset(path: str) -> List[str]:
+    '''
+    Function that return a list of image IDs
+    
+    :param path: str that represents path to txt file with image IDs
+    '''
     with open(path, 'r') as file:
         doc = file.read()
     return [txt.rsplit('.jpg')[0] for txt in doc.split('\n') if txt!='']
